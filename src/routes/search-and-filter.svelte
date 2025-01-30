774b1e4bc4a678ac86df13363300213c3832bc26
Daniel Pechersky <danny.pechersky@gmail.com>
2025-01-30 12:49:05 -0500

    Improve spacing between the search and filter bar

--- src/routes/search-and-filter.svelte
+++ src/routes/search-and-filter.svelte
@@ -14,8 +14,8 @@
 	} = $props();
 </script>
 
-<div class="flex flex-wrap gap-y-12 [&>*]:h-16">
-	<div class="relative theme-input w-[28rem]">
+<div class="flex flex-wrap gap-y-12 gap-x-4 [&>*]:h-16">
+	<div class="relative theme-input w-[28rem] mr-auto">
 		<Icon data={faSearch} class="absolute pointer-events-none my-auto top-0 bottom-0 left-8" />
 
 		<input
@@ -25,7 +25,6 @@
 			class="w-full h-full theme-element placeholder-current pl-16 pr-2 shadow rounded placeholder:font-light placeholder:text-sm"
 		/>
 	</div>
-	<div class="lg:mx-auto"></div>
 	<div class="relative w-64">
 		<Icon data={faAngleDown} class="absolute pointer-events-none my-auto top-0 bottom-0 right-6" />
 
